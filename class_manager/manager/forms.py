71ffbb0cc41865c26be2d22aac4b28942f4d45f8
Hui Wen Weng <hweng40@stuy.edu>
2024-04-15 13:59:38 -0400

    finished all views on django side, fixed model errors

--- class_manager/manager/forms.py
+++ class_manager/manager/forms.py
@@ -5,7 +5,15 @@ from .models import Course
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
-        fields = ['name', 'teacher']
+        fields = ['name', 'teacher', 'students']
         widgets = {
-
+        }
+        
+from .models import Student
+  
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['name', 'osis', 'grade', 'gpa']
+        widgets = {
         }
