3b99b9f14cf6a0447b5c17ceb69ee962e3340462
Calvin Pun <cpun40@stuy.edu>
2024-04-16 22:56:49 -0400

    merge

--- class_manager_vue/src/apps/course_detail/CourseDetail.vue
+++ class_manager_vue/src/apps/course_detail/CourseDetail.vue
@@ -3,6 +3,14 @@
         This is the course details coming from django, displayed in vue. <br>
     </div>
 
+    <div>
+        <form method="post" class="form"></form>
+        <input type="hidden" name="csrfmiddlewaretoken" v-bind:value="csrf_token" /> 
+        <p>
+        <label for="id_name">Name:</label>
+        <input type="text" name="name" value="" maxlength="100" required="" id="id_name" />
+      </p>
+    </div>
 </template>
     
 <script>
---
+++ class_manager_vue/src/apps/course_edit/courseEdit.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        This is the course edit coming from django, displayed in vue. <br>
+    </div>
+
+</template>
+    
+<script>
+    export default { 
+        name: 'App', 
+        components: {
+        },
+        data: function() {
+            return {
+            }
+        },
+        methods: {
+        }, 
+    }
+</script>
+
